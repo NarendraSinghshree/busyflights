25658837a9c3815fcb5cf7598f19dc6bf938dd27
narendra.singh <narendra.singh@LTSIPL099-Narender.SIPL.local>
2017-06-29 21:35:08 +0530

    Ignoring Test case.

--- src/test/java/com/travix/medusa/busyflights/BusyFlightsApplicationTests.java
+++ src/test/java/com/travix/medusa/busyflights/BusyFlightsApplicationTests.java
@@ -1,5 +1,6 @@
-/*package com.travix.medusa.busyflights;
+package com.travix.medusa.busyflights;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,10 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BusyFlightsApplicationTests {
-
+	@Ignore
 	@Test
 	public void contextLoads() {
 	}
 
 }
-*/
